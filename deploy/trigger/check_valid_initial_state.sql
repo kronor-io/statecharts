6f15505794660f1764515b015a40d20fe00e5b35
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2021-07-07 13:04:35 +0200

    make the initial state check a for each statement trigger
    This allows for easier deleting of specific statecharts

--- deploy/trigger/check_valid_initial_state.sql
+++ deploy/trigger/check_valid_initial_state.sql
@@ -4,15 +4,26 @@ BEGIN;
   create or replace function fsm.trig_check_valid_initial_state() returns trigger as
   $$
     declare
-        total_initial int;
+        chart bigint;
+        total_initial bigint;
     begin
-      select count(*) into total_initial
-      from fsm.get_initial_state(coalesce(NEW.statechart_id, OLD.statechart_id));
+      for chart in
+        select distinct statechart_id
+        from changed
+        where exists (select 1 from fsm.statechart where id = changed.statechart_id)
+      loop
+          select count(*) into total_initial
+          from fsm.state
+          where statechart_id = chart
+            and is_initial
+            and parent_id is null;
 
-      if total_initial <> 1
-      then
-        raise exception 'exactly one initial state is required per statechart';
-      end if;
+          if total_initial is distinct from 1
+          then
+            raise exception 'exactly one initial state is required per statechart';
+          end if;
+
+      end loop;
 
       return null;
     end;
@@ -24,37 +35,25 @@ BEGIN;
   drop trigger if exists check_valid_initial_state_delete on fsm.state;
   reset client_min_messages;
 
-  -- By making the triggers INITIALLY DEFERRED, we tell PostgreSQL
-  -- to check the condition at COMMIT time. This means that inside a
-  -- single transaction we are allowed to be inconsistent, as long as
-  -- the final result remains consistent.
-
-  create constraint trigger check_valid_initial_state_insert
+  create trigger check_valid_initial_state_insert
   after insert
   on fsm.state
-  deferrable initially deferred
-  for each row
-  when (NEW.is_initial and NEW.parent_id is null)
+  referencing new table as changed
+  for each statement
   execute function fsm.trig_check_valid_initial_state();
 
-  create constraint trigger check_valid_initial_state_update
+  create trigger check_valid_initial_state_update
   after update
   on fsm.state
-  deferrable initially deferred
-  for each row
-  when (
-    NEW.is_initial <> OLD.is_initial or
-    NEW.statechart_id <> OLD.statechart_id or
-    NEW.parent_id is distinct from OLD.parent_id
-  )
+  referencing new table as changed
+  for each statement
   execute function fsm.trig_check_valid_initial_state();
 
-  create constraint trigger check_valid_initial_state_delete
+  create trigger check_valid_initial_state_delete
   after delete
   on fsm.state
-  deferrable initially deferred
-  for each row
-  when (OLD.is_initial and OLD.parent_id is null)
+  referencing old table as changed
+  for each statement
   execute function fsm.trig_check_valid_initial_state();
 
 COMMIT;
