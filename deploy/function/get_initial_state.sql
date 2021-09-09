092767a08df8973896a19b3db0f559aea5f536d7
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2021-09-09 16:56:21 +0200

    Fix get_initial_state

--- deploy/function/get_initial_state.sql
+++ deploy/function/get_initial_state.sql
@@ -2,6 +2,8 @@
 
 BEGIN;
 
+  -- Returns all the chains of nodes that are marked as initial. The chain
+  -- is interrupted whenever there is a child marked a non-initial
   create or replace function fsm.get_initial_state(statechart bigint)
   returns setof fsm.state as
   $$
@@ -13,12 +15,23 @@ BEGIN;
           and parent_id is null
       )
       select * from root
+
       union
+
       select child_s.*
       from fsm.state child_s
       where child_s.statechart_id = statechart
         and child_s.is_initial
         and child_s.parent_path <@ (select node_path from root)
+        -- we make sure that the chaing of parents only contain initial nodes
+        and not exists (
+          select *
+          from fsm.state parent
+          -- <@ = left is descendant or equals to right
+          where child_s.node_path <@ parent.node_path
+            and not parent.is_initial
+        )
+
   $$ language sql stable strict parallel safe;
 
   comment on function fsm.get_initial_state(bigint) is $comment$
