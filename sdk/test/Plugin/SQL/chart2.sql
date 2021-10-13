12e9275960b884f84cddd152be2a5d4daa6e46b7
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2021-10-13 09:41:28 -0300

    chart2 info removed

--- sdk/test/Plugin/SQL/chart2.sql
+++ sdk/test/Plugin/SQL/chart2.sql
@@ -9,30 +9,30 @@ chart bigint;
 begin
 insert into fsm.statechart (name, version) values ('chart2', 0.1::semver) returning id into chart;
 insert into fsm.state (statechart_id, id, name, parent_id, is_initial, is_final, on_entry, on_exit) values
-(chart, 'settling', 'wating for full settlement', null, true, false, null, null),
-(chart, 'debt_collection', 'sent to debt collection', null, false, true, null, null),
-(chart, 'transferred_to_account', 'transferred to account', null, false, true, null, null),
-(chart, 'settled', 'settled', null, false, true, ('invoice', 'settled_action'), null),
-(chart, 'in_progress', 'waiting for minimum payment', 'settling', true, false, null, null),
-(chart, 'minimum_payment', 'received minimum payment', 'settling', false, false, null, null),
-(chart, 'created', 'created', 'in_progress', true, false, ('invoice', 'created_action'), null),
-(chart, 'due_date', 'due date', 'in_progress', false, false, ('invoice', 'due_action'), null),
-(chart, 'reminder1', 'first reminder date', 'in_progress', false, false, ('invoice', 'reminder1_action'), null),
-(chart, 'reminder2', 'second reminder date', 'in_progress', false, false, ('invoice', 'reminder2_action'), null),
-(chart, 'debt_collection_date', 'debt collection date', 'in_progress', false, true, ('invoice', 'debt_collection_action'), null),
-(chart, 'paid_more_than_min', 'paid enough to transfer to account', 'minimum_payment', true, false, ('invoice', 'paid_more_than_min_action'), null),
-(chart, 'transferring_to_account', 'starting the transfer to account', 'minimum_payment', false, true, ('invoice', 'transferring_to_account_action'), null);
+(chart, 's1', 's1n', null, true, false, null, null),
+(chart, 's11', 's11n', null, false, true, null, null),
+(chart, 's12', 's12n', null, false, true, null, null),
+(chart, 's13', 's13n', null, false, true, ('table1', 'action11'), null),
+(chart, 's2', 's2n', 's1', true, false, null, null),
+(chart, 's8', 's8n', 's1', false, false, null, null),
+(chart, 's3', 's3n', 's2', true, false, ('table1', 'action1'), null),
+(chart, 's4', 's4n', 's2', false, false, ('table1', 'action2'), null),
+(chart, 's5', 's5n', 's2', false, false, ('table1', 'action3'), null),
+(chart, 's6', 's6n', 's2', false, false, ('table1', 'action4'), null),
+(chart, 's7', 's7n', 's2', false, true, ('table1', 'action5'), null),
+(chart, 's9', 's9n', 's8', true, false, ('table1', 'action7'), null),
+(chart, 's10', 's10n', 's8', false, true, ('table1', 'action6'), null);
 insert into fsm.transition (statechart_id, event, source_state, target_state) values
-(chart, 'done.state.debt_collection_date', 'settling', 'debt_collection'),
-(chart, 'done.state.minimum_payment', 'settling', 'transferred_to_account'),
-(chart, 'invoice.settle', 'settling', 'settled'),
-(chart, 'invoice.pay.enough', 'in_progress', 'minimum_payment'),
-(chart, 'invoice.time.due', 'created', 'due_date'),
-(chart, 'invoice.time.reminder1', 'due_date', 'reminder1'),
-(chart, 'invoice.time.reminder2', 'reminder1', 'reminder2'),
-(chart, 'invoice.time.debt_collection', 'reminder2', 'debt_collection_date'),
-(chart, 'invoice.time.due', 'paid_more_than_min', 'transferring_to_account'),
-(chart, 'invoice.time.past_due', 'paid_more_than_min', 'transferring_to_account');
+(chart, 'bar.abc3', 's1', 's11'),
+(chart, 'bar.abc2', 's1', 's12'),
+(chart, 'foo.abc1', 's1', 's13'),
+(chart, 'foo.abc6', 's2', 's8'),
+(chart, 'foo.ev10', 's3', 's4'),
+(chart, 'foo.ev9', 's4', 's5'),
+(chart, 'foo.ev8', 's5', 's6'),
+(chart, 'foo.abc7', 's6', 's7'),
+(chart, 'foo.abc5', 's9', 's10'),
+(chart, 'foo.abc4', 's9', 's10');
 end
 $$;
 COMMIT;
