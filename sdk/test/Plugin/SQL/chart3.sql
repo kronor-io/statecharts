04d6f9a8c7f4a31b5cfed42a6f0a2b2be299e976
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2021-10-13 09:50:15 -0300

    extracted info from chart3

--- sdk/test/Plugin/SQL/chart3.sql
+++ sdk/test/Plugin/SQL/chart3.sql
@@ -9,26 +9,26 @@ chart bigint;
 begin
 insert into fsm.statechart (name, version) values ('chart3', 0.1::semver) returning id into chart;
 insert into fsm.state (statechart_id, id, name, parent_id, is_initial, is_final, on_entry, on_exit) values
-(chart, 'waiting_for_capture', 'waiting for capture', null, true, false, ('runtime', 'check_for_capture'), null),
-(chart, 'initializing', 'initialize payment', null, false, false, ('runtime', 'enqueue_payment_create_job'), null),
-(chart, 'waiting_for_payment', 'waiting for payment', null, false, false, ('runtime', 'enqueue_payment_polling_job'), null),
-(chart, 'paid', 'paid', null, false, true, ('runtime', 'promote_payment_to_marketplace'), null),
-(chart, 'error', 'error', null, false, true, null, null),
-(chart, 'declined', 'payment declined', null, false, true, null, null),
-(chart, 'cancelled', 'payment cancelled', null, false, true, null, null),
-(chart, 'processing', 'processing the payment', 'waiting_for_payment', true, false, null, null),
-(chart, 'cancelling', 'cancel payment request', 'waiting_for_payment', false, false, ('runtime', 'enqueue_payment_cancel_job'), null);
+(chart, 's1', 's1n', null, true, false, ('table1', 'action5'), null),
+(chart, 's2', 's2n', null, false, false, ('table1', 'action4'), null),
+(chart, 's3', 's3n', null, false, false, ('table1', 'action3'), null),
+(chart, 's6', 's6n', null, false, true, ('table1', 'action1'), null),
+(chart, 's7', 's7n', null, false, true, null, null),
+(chart, 's8', 's8n', null, false, true, null, null),
+(chart, 's9', 's9n', null, false, true, null, null),
+(chart, 's4', 's4n', 's3', true, false, null, null),
+(chart, 's5', 's5n', 's3', false, false, ('table1', 'action2'), null);
 insert into fsm.transition (statechart_id, event, source_state, target_state) values
-(chart, 'payment.initialize', 'waiting_for_capture', 'initializing'),
-(chart, 'payment.created', 'initializing', 'waiting_for_payment'),
-(chart, 'payment.cancel', 'initializing', 'cancelled'),
-(chart, 'payment.error', 'initializing', 'error'),
-(chart, 'payment.paid', 'waiting_for_payment', 'paid'),
-(chart, 'payment.declined', 'waiting_for_payment', 'declined'),
-(chart, 'payment.cancelled', 'waiting_for_payment', 'cancelled'),
-(chart, 'payment.error', 'waiting_for_payment', 'error'),
-(chart, 'payment.cancel', 'processing', 'cancelling'),
-(chart, 'payment.cancel.failed', 'cancelling', 'processing');
+(chart, 'ev1', 's1', 's2'),
+(chart, 'ev2', 's2', 's3'),
+(chart, 'ev3', 's2', 's9'),
+(chart, 'ev4', 's2', 's7'),
+(chart, 'ev5', 's3', 's6'),
+(chart, 'ev6', 's3', 's8'),
+(chart, 'ev7', 's3', 's9'),
+(chart, 'ev8', 's3', 's7'),
+(chart, 'ev9', 's4', 's5'),
+(chart, 'ev10', 's5', 's4');
 end
 $$;
 COMMIT;
