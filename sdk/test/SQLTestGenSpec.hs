9cac2b4b5b51a3a6464ccfcd0d5c5e0bc34d74d0
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2021-11-01 16:46:20 -0300

    placeholder and ideas for tests

--- sdk/test/SQLTestGenSpec.hs
+++ sdk/test/SQLTestGenSpec.hs
@@ -1,3 +1,49 @@
--- in here there will be the tests regarding sql test gen.
--- we are going to use one of the scxmls, parse it, and generate sql tests for it.
--- then we compare this tests with the manually written sql test file.
+
+-- | Example of how one path of the invoice_flow might look.]
+-- We can use this to create SQL tests.
+-- We can use the number of tests to specify the pgTap "prove(n)"
+examplePath :: Path -- TODO also maybe do not call it path since it might be confusing
+-- call it transition record, maybe?
+examplePath =
+  -- TODO instead of doing it like this, use a single constructor for this cluster
+  -- of 3 things
+  [ NoNewTransition
+  , State "settling/in_progress/created"
+  , Action "invoice.created_action"
+
+  , Trans "invoice.time.soft_reminder"
+  , State "settling/in_progress/in_progress_soft_reminder"
+  , Action "invoice.soft_reminder_action"
+
+  , Trans "invoice.time.due"
+  , State "settling/in_progress/due_date"
+  , Action "invoice.due_date_action"
+
+  , Trans "invoice.time.reminder1"
+  , State "settling/in_progress/reminder1" 
+  , Action "invoice.reminder1_action"
+
+  , Trans "invoice.time.reminder2"
+  , State "settling/in_progress/reminder2" 
+  , Action "invoice.reminder2_action"
+
+  , Trans "invoice.time.debt_collection"
+  , State "settling/in_progress/debt_collection_date"
+  , NoNewAction
+
+  , Trans "invoice.pay.enough"
+  , State "settling/minimum_payment/paid_more_than_min"
+  , Action "invoice.paid_more_than_min_action"
+
+  , Trans "invoice.time.soft_reminder"
+  , State "settling/minimum_payment/minimum_payment_soft_reminder"
+  , Action "invoice.soft_reminder_action"
+
+  , Trans "invoice.time.due"
+  , State "settling/minimum_payment/transferring_to_account"
+  , Action "invoice.transferring_to_account_action"
+
+  , Trans "invoice.settle"
+  , State "settled"
+  , Action "invoice.invoice_settled_action"
+  ]
--- sdk/test/example_sql_test.sql
+++ sdk/test/example_sql_test.sql
@@ -1,4 +1,4 @@
--- manually written, but mimicking the generated code, will be here
+
 BEGIN;
 -- the general flow, assuming the simplest version
 -- is that we first we initiate a new a machine.
@@ -7,4 +7,34 @@ BEGIN;
 -- then we start testing the paths individually
 -- by using notify_state_machine and then checking in which state are we, and in case there was actions to be handled, if the actions that we intercepeted are the same ones. this mostly still only tests the state chart machinery.
 -- when we extend the scxml then we will be able to test stuff that are relevant to us, lets call it business logic per se instead of just testing the state chart machinery.
+
+-- TODO idea, create a separate table for when the test are to run so we can do inserts from inside the intercepeted handle_machine_events fn.
+
+-- TODO alternatively we may pass the is or ok fn inside the body of the intercepeted fn. will it work?
+
+create or replace table intercepted ...
+create or replace function last_intercepted()....
+
+create or replace function fsm.handle_machine_events()....
+ $$
+  insert into intercepted () values .....
+ $$
+
+ --select notify_state_machine(...);
+ select notify_state_machine(1,:mid, '', ''::jsonb);
+ with foo as foo (select last_intercepted()) select is(:foo,'invoice.created_action');
+
+ select notify_state_machine(1,:mid, '', ''::jsonb);
+ select fsm.state_machine_state where ...;
+ with foo as foo (select last_intercepted()) select is(:foo,'invoice.soft_reminder_action');
+
+ select notify_state_machine(1,:mid, '', ''::jsonb);
+ select fsm.state_machine_state where ...;
+ with foo as foo (select last_intercepted()) select is(:foo,'invoice.due_date_action');
+
+ -- etc
+
+  -- create the machine of name invoice_flow with lastest chart
+  -- run first cluster, meaning do the transition, check the state, check the action
+
 ROLLBACK;
