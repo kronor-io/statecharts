86c0e4f1d2ee144d119de358c2e491a2f17746a6
Axel Ulmestig <axel.ulmestig@gmail.com>
2021-12-01 15:12:31 +0100

    add verify for remaining plpgsql functions

--- verify/function/is_state_active.sql
+++ verify/function/is_state_active.sql
@@ -2,6 +2,27 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+  select  has_function_privilege('fsm.is_state_active(bigint, bigint, text)', 'execute');
+
+  do $$
+    declare
+      err text;
+    begin
+
+      select string_agg(
+        errors, '
+        ') into err -- the string with a new line is on purpose
+
+      from
+
+      plpgsql_check_function('fsm.is_state_active(bigint, bigint, text)') errors;
+
+      if err <> ''
+      then
+        raise exception '%', err;
+      end if;
+
+    end;
+    $$;
 
 ROLLBACK;
