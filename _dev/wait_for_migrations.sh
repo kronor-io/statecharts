5545931752b6004fb4e3a2e5326e3e4999f8957b
Mrinal <mrinal1892005@gmail.com>
2025-02-07 15:06:06 +0100

    Fix payload in done transitions (#36)
    The payload would be set to `{}` in `done.state.*` transitions and states after the current state wouldn't get the actual event payload which would break the flow down the line if any states actually used the data from the payload
    
    Also added a test to verify payload for parallel states
    
    ---------
    
    Co-authored-by: Axel Ulmestig <AxelUlmestig@users.noreply.github.com>

--- _dev/wait_for_migrations.sh
+++ _dev/wait_for_migrations.sh
@@ -3,26 +3,26 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-until [ ! -z "$(docker-compose exec -T postgres psql -c 'select * from fsm.statechart' 2>/dev/null)" ]
+until [ ! -z "$(docker compose exec -T postgres psql -c 'select * from fsm.statechart' 2>/dev/null)" ]
 
 do
   echo "Waiting for database boot sequence........"
   sleep 1s
 done
 
-until [ ! -z "$(docker-compose exec -T postgres sqitch status 2>&1 | grep "Nothing to deploy")" ];
+until [ ! -z "$(docker compose exec -T postgres sqitch status 2>&1 | grep "Nothing to deploy")" ];
 do
   echo "Waiting for migrations to be applied......"
   sleep 1s
 
   # If migrations are not automatically running, run them now
-  if [ ! -z "$(docker-compose exec -T postgres sqitch status 2>&1 | grep "No changes deployed")" ]
+  if [ ! -z "$(docker compose exec -T postgres sqitch status 2>&1 | grep "No changes deployed")" ]
     then
-      COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec postgres sqitch --chdir statecharts deploy
-      COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec postgres sqitch deploy
+      COMPOSE_INTERACTIVE_NO_CLI=1 docker compose exec postgres sqitch --chdir statecharts deploy
+      COMPOSE_INTERACTIVE_NO_CLI=1 docker compose exec postgres sqitch deploy
   fi
 
-  if [ ! -z "$(docker-compose exec -T postgres sqitch status 2>&1 | grep "Cannot find this change")" ]
+  if [ ! -z "$(docker compose exec -T postgres sqitch status 2>&1 | grep "Cannot find this change")" ]
     then
       echo -e "${RED}"
       echo -e "##################################################################"
