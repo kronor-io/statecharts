85c5b4846edb3b67a4e9931e2f477b18957502db
pranaysashank <pranaysashank@gmail.com>
2025-11-05 19:07:41 +0530

    Add an executable generate-chart to generate sql files from scxml.
    - Also add flake.nix, so generate-chart can now be installed with
      `nix profile add --accept-flake-config github:kronor-io/statecharts`

---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  description = "statecharts";
+  nixConfig = {
+    extra-substituters = "https://pranaysashank.cachix.org";
+    extra-trusted-public-keys = "pranaysashank.cachix.org-1:VeqW46y6BVO74w4ViwzeWqSpDqxuWxtC2DO2zoe9rzc=";
+  };
+  inputs = {
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    haskell-nix.url = "github:kronor-io/haskell.nix/wip-nixpkgs-ghc";
+    haskell-nix.inputs.hackage.follows = "hackage-nix";
+    haskell-nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    hackage-nix.url = "github:kronor-io/hackage.nix";
+    hackage-nix.flake = false;
+  };
+  outputs =
+    { self
+    , git-hooks
+    , haskell-nix
+    , hackage-nix
+    , nixpkgs
+    }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      mergeAttrs = x: y: x // y;
+
+      foldr = op: nul: list:
+        let
+          len = builtins.length list;
+          fold' = n:
+            if n == len
+            then nul
+            else op (builtins.elemAt list n) (fold' (n + 1));
+        in
+        fold' 0;
+
+      foldAttrs =
+        op:
+        nul:
+        list_of_attrs:
+        foldr
+          (n: a:
+          foldr
+            (name: o:
+            o // { ${name} = op n.${name} (a.${name} or nul); }
+            )
+            a
+            (builtins.attrNames n)
+          )
+          { }
+          list_of_attrs;
+      eachSystem = f: foldAttrs mergeAttrs { }
+        (map (s: builtins.mapAttrs (_: v: { ${s} = v; }) (f s)) supportedSystems);
+    in
+    eachSystem (system:
+    let
+      haskellNix = import haskell-nix {
+        inherit system;
+        sourcesOverride = {
+          hackage = hackage-nix;
+        };
+      };
+
+      pkgs = import nixpkgs (
+        {
+          inherit system;
+          overlays = [ haskellNix.overlay ];
+        }
+      );
+      pristinePkgs = import nixpkgs (
+        {
+          inherit system;
+        }
+      );
+      compiler-nix-name = "ghc9122";
+
+      statechartProject = pkgs.haskell-nix.project {
+        src = ./sdk;
+        modules = (if system == "x86_64-darwin" || system == "aarch64-darwin" then [] else [{
+          dontPatchELF = false;
+          dontStrip = false;
+        }]) ++ [{ doHaddock = false; }];
+
+        inherit compiler-nix-name;
+        cabalProjectFreeze = builtins.readFile ./sdk/cabal.project.freeze;
+        supportHpack = false;
+      };
+
+      shell = statechartProject.shellFor {
+
+        withHoogle = false;
+
+        shellHook = ''
+          ${self.checks.${system}.pre-commit-check.shellHook}
+        '';
+      };
+
+    in
+    {
+      checks = {
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+            fourmolu.enable = true;
+          };
+        };
+      };
+      packages = {
+        default = statechartProject.statechart-sdk.components.exes.generate-chart;
+        freezeFile = statechartProject.plan-nix.freeze;
+      };
+      devShells.default = shell;
+    }
+    );
+}
---
+++ sdk/app/GenerateChart.hs
@@ -0,0 +1,104 @@
+{-# language OverloadedRecordDot, BlockArguments #-}
+
+module GenerateChart (main) where
+
+import Path
+import Path.IO hiding (findExecutable)
+import RIO
+import RIO.ByteString qualified as BS
+import RIO.ByteString.Lazy qualified as LBS
+import RIO.Text (pack)
+import Statechart.CodeGen.SQL
+import Statechart.SCXML as SCXML
+import System.Environment (getArgs)
+import Options.Applicative
+import System.FilePath qualified as F ((</>))
+import System.Process.Typed (proc, runProcess_)
+import Statechart.CodeGen.Visualization (generatePlantuml)
+import System.Directory (findExecutable)
+
+data Options = Options
+  { project      :: String
+  , databaseDir  :: String
+  , files        :: [String]
+  }
+  deriving (Show)
+
+optionsParser :: Parser Options
+optionsParser = Options
+      <$> strOption
+          ( long "project"
+         <> metavar "PROJECT"
+         <> help "Sqitch project that should contain the statecharts" )
+      <*> strOption
+          ( long "database-dir"
+         <> metavar "DATABASE_DIR"
+         <> showDefault
+         <> value "database"
+         <> help "Sqitch project that should contain the statecharts" )
+      <*> option auto
+          ( long "files"
+         <> help "Generate only these statecharts"
+         <> value []
+         <> metavar "FILES" )
+
+
+opts :: ParserInfo Options
+opts = info (optionsParser <**> helper)
+       ( fullDesc
+         <> progDesc "Generate sqitch based sql migrations from scxml files"
+         <> header "generate-chart")
+
+main :: IO ()
+main = do
+    options <- execParser opts
+
+    chartDir <- resolveDir'  (options.databaseDir F.</> "deploy/statechart")
+    deployDir <- resolveDir' (options.databaseDir F.</> "deploy/statechart")
+    verifyDir <- resolveDir' (options.databaseDir F.</> "verify/statechart")
+    revertDir <- resolveDir' (options.databaseDir F.</> "revert/statechart")
+
+    scxmls <- case options.files of
+        [] -> map (first (chartDir </>)) <$> readSCXMLfiles chartDir
+        _ -> for options.files $ \fp -> do
+            af <- resolveFile' fp
+            a <- BS.readFile (fromAbsFile af)
+            case parse $ LBS.fromStrict a of
+                Left e -> error . show $ e
+                Right p -> return (af, p)
+
+    let charts = map snd scxmls
+
+    -- Generation
+    let sqls = generateSQL (pack $ options.project <> ":statechart") charts
+        verifySqls = generateSQLVerify (pack $ options.project <> ":statechart") charts
+        revertSqls = generateSQLRevert (pack $ options.project <> ":statechart") charts
+
+    -- Writing to disk
+    writeSQLs deployDir sqls
+    writeSQLs verifyDir verifySqls
+    writeSQLs revertDir revertSqls
+
+    needThen "plantuml" "Skipping generation of svg as plantuml is not found in PATH" do
+      for_ scxmls $ \(path, chart) -> do
+          umlPath <- addExtension ".pml" path
+          let umlCode = generatePlantuml chart
+          ensureDir $ parent umlPath
+          writeFileUtf8 (fromAbsFile umlPath) umlCode
+          runProcess_ $ proc "plantuml" ["-tsvg", fromAbsFile umlPath]
+
+need :: String -> IO ()
+need exe = do
+    mSqitchExe <- findExecutable exe
+    when (isNothing mSqitchExe) $ do
+      putStrLn ("Aborting program as executable " <> exe <> " not found")
+      exitFailure
+
+needThen :: String -> String -> IO a -> IO ()
+needThen exe notExistsMsg whenExists = do
+    mSqitchExe <- findExecutable exe
+    case mSqitchExe of
+      Just _ -> do
+        void whenExists
+      Nothing -> do
+        putStrLn notExistsMsg
