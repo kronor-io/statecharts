0d049ec2775b28a65420dae4588483b017c1b7ad
Axel Ulmestig <axel.ulmestig@gmail.com>
2026-01-28 19:51:06 +0100

    proper verify scripts

--- pg_statecharts/src/gen_charts.rs
+++ pg_statecharts/src/gen_charts.rs
@@ -319,9 +319,62 @@ r#"-- Verify {}:statechart/{}-{} on pg
 -- FILE AUTOMATICALLY GENERATED. MANUAL CHANGES MIGHT BE OVERWRITTEN
 
 BEGIN;
+
+-- Verify that the statechart is added
+select 1 / count(*)
+from fsm.statechart
+where
+    name = '{}'
+    and version = {}::semver;
+
+-- Verify that the functions that the statechart depends on exist
+do $$
+declare
+    missing_funcs_count_ int;
+    missing_funcs_ text;
+begin
+
+select
+  string_agg(distinct format('%s.%s', schema_name, function_name), ', '),
+  count(*)
+into
+  missing_funcs_,
+  missing_funcs_count_
+from fsm.statechart
+join fsm.state
+    on state.statechart_id = statechart.id
+, lateral unnest(on_entry || on_exit)
+where
+  statechart.name = '{}'
+  and statechart.version = {}::semver
+  and not exists (
+    select 1
+    from pg_proc p
+    join pg_namespace n
+      on p.pronamespace = n.oid
+    where
+      n.nspname = schema_name
+      and p.proname = function_name
+      and p.pronargs = 1
+      and p.proargtypes[0] = 'fsm_event_payload'::regtype::oid
+  );
+
+if missing_funcs_count_ > 0 then
+  raise exception
+    $err$
+
+    One or more missing or invalid functions: %
+    All functions must take exactly one argument of the type fsm_event_payload
+
+    $err$, missing_funcs_;
+end if;
+
+end
+$$;
+
 ROLLBACK;
 "#,
-        project, scxml.name, scxml.version
+        project, scxml.name, scxml.version, scxml.name, scxml.version, scxml.name, scxml.version
     );
 
     return Migration {
