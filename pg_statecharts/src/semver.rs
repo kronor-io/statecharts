0dab2077492a77d938cde0e21f827ad65683dd99
Axel Ulmestig <axel.ulmestig@gmail.com>
2026-02-16 21:39:41 +0100

    move semver.sql file into semver.rs

--- pg_statecharts/src/semver.rs
+++ pg_statecharts/src/semver.rs
@@ -31,7 +31,7 @@ impl PgVarlenaInOutFuncs for Semver {
     fn output(&self, buffer: &mut StringInfo) {
         let formatted_patch = match self.patch {
             Some(p) => format!(".{}", p),
-            None => "".to_string()
+            None => "".to_string(),
         };
         buffer.push_str(&format!("{}.{}{}", self.major, self.minor, formatted_patch));
     }
@@ -115,12 +115,123 @@ fn hash_semver(semver: PgVarlena<Semver>) -> i32 {
     let mut hash: i32 = 17;
     hash = hash.wrapping_mul(31).wrapping_add(semver.major);
     hash = hash.wrapping_mul(31).wrapping_add(semver.minor);
-    hash = hash.wrapping_mul(31).wrapping_add(semver.patch.unwrap_or(0));
+    hash = hash
+        .wrapping_mul(31)
+        .wrapping_add(semver.patch.unwrap_or(0));
     hash
 }
 
+/*
 extension_sql_file!(
     "../sql/pg_statecharts--0.1.0--semver.sql",
     name = "semver_type",
+    requires = [Semver, semver_lt, semver_ne, hash_semver]
+);
+*/
+
+extension_sql!(
+    r#"
+        create operator < (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_lt,
+            commutator = >,
+            negator = >=,
+            restrict = scalarltsel,
+            join = scalarltjoinsel
+        );
+        
+        create operator <= (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_le,
+            commutator = >=,
+            negator = >,
+            restrict = scalarltsel,
+            join = scalarltjoinsel
+        );
+        
+        create operator = (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_eq,
+            commutator = =,
+            negator = <>,
+            restrict = eqsel,
+            join = eqjoinsel
+        );
+        
+        create operator <> (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_ne,
+            commutator = <>,
+            negator = =,
+            restrict = neqsel,
+            join = neqjoinsel
+        );
+        
+        create operator >= (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_ge,
+            commutator = <=,
+            negator = <,
+            restrict = scalarltsel,
+            join = scalarltjoinsel
+        );
+        
+        create operator > (
+            leftarg = semver,
+            rightarg = semver,
+            procedure = semver_gt,
+            commutator = <,
+            negator = <=,
+            restrict = scalarltsel,
+            join = scalarltjoinsel
+        );
+        
+        --
+        -- the btree indexing operator class.
+        --
+        
+        create operator class semver_ops
+            default for type semver using btree as
+            operator    1   <  (semver, semver),
+            operator    2   <= (semver, semver),
+            operator    3   =  (semver, semver),
+            operator    4   >= (semver, semver),
+            operator    5   >  (semver, semver),
+            function    1   semver_cmp(semver, semver);
+        
+        --
+        -- the hash indexing operator class.
+        -- note: you cannot have two operator classes with the same name!
+        -- use a different name for hash operator class
+        --
+        
+        create operator class semver_hash_ops
+            default for type semver using hash as
+            operator    1   =  (semver, semver),
+            function    1   hash_semver(semver);
+        
+        create cast (text as semver)
+        with function semver_from_text(text)
+        as implicit;
+        
+        create or replace function to_semver(text_semver text)
+          returns semver as
+          $$
+          declare
+            dot_count integer;
+          begin
+            dot_count := length(text_semver) - length(replace(text_semver, '.', ''));
+        
+            -- pads with '.0' if the provided value has fewer than two dots
+            return (text_semver || repeat('.0', 2 - dot_count))::semver;
+          end;
+          $$ language plpgsql;
+    "#,
+    name = "semver_operators",
     requires = [Semver, semver_lt, semver_ne, hash_semver]
 );
